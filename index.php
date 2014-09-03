d6923689fb65a8b42da048838ea410f3962e7495
George Wu <georgewu@college.harvard.edu>
2014-09-02 23:18:24 -0400

    adding dates to website for fall recruitment

--- index.php
+++ index.php
@@ -21,6 +21,13 @@ $id = $_SESSION["id"];
     		<div class="center">
 				<img src="images/aadt_logo.png" />
 				<p>Welcome to the website of the Harvard Asian American Dance Troupe (AADT), a student-run organization at Harvard College! Please join us as we explore the world of Asian culture and dance.</p>
+	<h2><b>Important dates for Fall 2014<b> | September 2, 2014</h2><br/>
+	Fri, Sept 5 - Extracurricular Fair - 4PM - 7PM @ SOCH Lawn
+	Sat, Sept 6 - Introductory Meeting for Freshmen - 1PM - 2PM @ Sever 103
+	Sun, Sept 7 - First General Rehearsal - 2PM - 4PM @ 74 Mt. Auburn St
+	Sun, Sept 14 - Fall Showcase - 2PM - 4PM @ 74 Mt. Auburn St
+	Sun, Sept 21 - Fall Shopping - All day @ 74 Mt. Auburn St
+
 	<h2><b>Join AADT 2014-2015!</b> | April 21, 2014 </h2><br/>
 	<iframe width="550" height="315" src="http://www.youtube.com/embed/mDLDJMJi05c" frameborder="0" allowfullscreen></iframe>
 	
